839c0e65d9c1337b0bee85cc9a908d6a6111ff9d
Krystle Taniguchi <ktanig87@gmail.com>
2022-04-11 08:14:28 -0600

    Add project files.

---
+++ Notizen.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Notizen.WebMVC.Startup))]
+namespace Notizen.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
